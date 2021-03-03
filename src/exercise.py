3074b568fe43a0b2b8c12047de220e4cc256a8a1
MrSullivan <rsullivan@stcadocs.com>
2021-03-03 12:43:12 +0000

    removed gap after ?

--- src/exercise.py
+++ src/exercise.py
@@ -1,6 +1,6 @@
 def main():
     #write your code below this line
-    age = int(input("How old are you? "))
+    age = int(input("How old are you?"))
 
     if (age <0 or age >120):
       print("Impossible!")
